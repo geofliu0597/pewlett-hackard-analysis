2ad4a63471aee6591aeef38792066e1cd64c1e46
geofliu0597 <geofliu0597@gmail.com>
2022-06-28 15:25:51 +0800

    retirement info

--- schema.sql
+++ schema.sql
@@ -17,22 +17,41 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+create table dept_emp (
+    emp_no int not null,
+    dept_no varchar(4) not null,
+    from_date date not null,
+    to_date date not null,
+    foreign key (emp_no) references employees (emp_no),
+    foreign key (dept_no) references departments (dept_no),
+    primary key (emp_no, dept_no)
+);
+
+create table titles (
+    emp_no int not null,
+    title varchar(40) not null,
+    from_date date not null,
+    to_date date not null,
+    foreign key (emp_no) references employees (emp_no),
+    primary key (emp_no, title, from_date)
+);
+
+
